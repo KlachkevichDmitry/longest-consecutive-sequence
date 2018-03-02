bc8b1dac89af4d3278d105b278773a07fde96fb6
Klachkevich Dmitry <klachkevich.dmitry@gmail.com>
2018-03-02 13:05:34 +0300

    longest consecutive sequence: all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let longest=0;
+  const arr=new Set(array);
+  for (let num of arr) {
+		if (arr.has(num)) {
+			let currentNum=num;
+			let currentSteak=1;
+				while (arr.has(currentNum+1)){
+					currentNum++;
+					currentSteak++;}
+		longest=Math.max(longest,currentSteak);
+		}
+  }
+  return longest;			
 }
